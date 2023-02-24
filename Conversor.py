d0bc679c16fa066b6d7173339e789bb2a2349be4
isaiduranc2003 <za210110253@zapopan.tecmm.edu.mx>
2023-02-23 19:20:31 -0600

    first commit

---
+++ Conversor.py
@@ -0,0 +1,20 @@
+# aplicacion para convertir pies a metros usando tkinter
+#Durán Calderón Isaí Gamaliel
+#23/febrero/2023
+
+from tkinter import *
+from tkinter import ttk
+
+
+#init funciona como constructor
+class Conversor:
+    #tipo constructor de la clase
+    def __init__(self,raiz):
+        #crear atributos
+        #poner titulo a la ventana
+        raiz.title("Pies a metros")
+
+
+if __name__=="__main__":
+    print("Este es el archivo principal.")
+    print("Nada mas se mostrara esto si es el archivo principal.")
---
+++ Principal.py
@@ -0,0 +1,7 @@
+from tkinter import *
+from tkinter import Tk
+from Conversor import Conversor
+
+raiz = Tk()
+Conversor(raiz)
+raiz.mainloop()
